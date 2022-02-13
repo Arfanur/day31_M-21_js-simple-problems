760b4007b985746f4356ad948dcc1e5ec2305b0b
Arfanur Rahman <arfanur03@gmail.com>
2022-02-13 17:44:21 +0600

    4_sum

---
+++ 4_sum.js
@@ -0,0 +1,19 @@
+const numbers = [44, 23, 34, 32, 54, 5, 78];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    // console.log(i, element);
+    sum = sum + element;
+}
+// console.log(sum);
+
+function arayTotal(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+const total = arayTotal([32, 45, 67]);
+console.log(total);

e65bd01600982cda6e5c1e6e7df3bec8bc4ac85a
Arfanur Rahman <arfanur03@gmail.com>
2022-02-14 12:49:21 +0600

    7th video

--- 6_fibonacci.js
+++ 6_fibonacci.js
@@ -10,11 +10,43 @@ nth = (n-1)th + (n-2)th
 ith = (i-1)th + (i-2)th
 */
 
-
+/* 
 const fibo = [0, 1];
 for (let i = 2; i <= 10; i++) {
-    // nth = (n-1)th + (n-2)th
-    // ith = (i-1)th + (i-2)th
     fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
 console.log(fibo);
+ */
+
+/* 
+function fibonacciSeries(num) {
+    const fibo = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+
+const fiboSeries = fibonacciSeries([234]);
+console.log(fiboSeries);
+ */
+
+
+function fibonacciSeries(num) {
+    if (typeof num != 'number') {
+        return 'Please give a number';
+    }
+    if (num < 2) {
+        return 'Please enter a positive a number greater than 1';
+    }
+
+    // console.log(typeof num);
+    const fibo = [0, 1];
+    for (let i = 2; i < num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+
+const fiboSeries = fibonacciSeries(13);
+console.log(fiboSeries);
